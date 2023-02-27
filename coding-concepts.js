7ea4d03e662e1ff4dda281e557adba3586b08d6d
JacobR96 <jrosswalks@gmail.com>
2023-02-27 03:39:28 -0800

    completed assessment

--- coding-concepts.js
+++ coding-concepts.js
@@ -10,8 +10,8 @@
 const cohort = "Alpha 2023"
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will take and seperate each word of the string and make them into a array .
+// b) Verify and explain: Takes and divides a String into an ordered list of substrings and then puts these substrings into an array.
 
 // --------------------2) What will this log?
 
@@ -20,24 +20,24 @@ const greeter = (name) => {
 }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:this will log Hello, LEARN Student
+// b) Verify and explain: name is a pareamter that can have values passed thru . in this case the value was learn stuedents and plus the string hello (all returned using string interpulation)
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
 // console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: these numbers will come back doubled [8,10,12,14,16]
+// b) Verify and explain:the parameter (number) will use the array values as argumnet in number * 2 
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
 // console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: this will filter out any number that is not %2 so it will be [11, 13, 15]
+// b) Verify and explain: the % 2  !== 0 and the filter method will work together and return a subset oof number that are not % 2 ===0 which is odd.
 
 // --------------------5) What will this log?
 
@@ -49,5 +49,5 @@ const myCodingSkills = {
 }
 // console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:this will log JavaScript
+// b) Verify and explain: this is like a file path , if you wanna get to a certian folder you need to know the path that its in . for this its in myCodingSkills in a folder called laugauges and its the first file [0] 
